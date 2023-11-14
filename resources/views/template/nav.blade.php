c1ca7d91e66e1aef87bfe36e0daea90f21684102
Syakigrodan <syakirana.2005@gmail.com>
2023-11-14 07:46:02 +0700

    Ganti Jadi SaintekFood

--- resources/views/template/nav.blade.php
+++
@@ -1,8 +0,0 @@
-<li class="nav-item @if ($active == 'Barang') {
-    {{ 'active' }}
-} @endif">
-    <a class="nav-link" href="{{ url('barang') }}]">
-        <i class="fas fa-fw fa-cube"></i>
-        <span>Data</span>
-    </a>
-</li>
